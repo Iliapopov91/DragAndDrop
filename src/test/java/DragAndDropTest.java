02a83b9569d65c9625920b5e6777aa81ef1622d9
Илья <50883558+Iliapopov91@users.noreply.github.com>
2026-03-18 00:01:08 +0300

    выполнение задания dragAndDropTest

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,4 +1,48 @@
-package java;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class DragAndDropTest {
+
+    @BeforeAll
+    static void beforeAll() {
+
+        //Задаём разрешение браузера
+        Configuration.browserSize= "2560x1440";
+
+        //Задаём URL
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+
+        //Пропускаем полную загрузку страницы
+        Configuration.pageLoadStrategy = "eager";
+
+        //Оставляем браузер открытым
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    public void dragAndDropTest() {
+
+        //Открыть страницу
+        open("/drag_and_drop");
+
+        //Назначаем элементы
+        SelenideElement columnA = $("#column-a");
+        SelenideElement columnB = $("#column-b");
+
+        //drag and drop
+        columnA.dragAndDrop(DragAndDropOptions.to(columnB));
+
+        // Проверка, что элементы поменялись местами
+        $("#column-a header").shouldHave(text("B"));
+        $("#column-b header").shouldHave(text("A"));
+
+
+    }
 }
